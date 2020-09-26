72987d3963e229b8e14eef4882e1ca81919b3224
satvik <satvik213161@gmail.com>
2020-09-26 11:09:27 +0530

    Video Uploading fixes

--- functions/middleware/auth.js
+++ functions/middleware/auth.js
@@ -5,17 +5,17 @@ const firebase = require('./firebaseFunc.js');
 
 const auth = async (req, res, next) => {
     try {   
-            // var user;
-            // if(req.headers.uid){
+            var user;
+            if(req.headers.uid){
                 const uid = await req.header('uid').replace('Bearer ', '');
                 user = await firebase.admin.auth().getUser(uid);
                 
-            // }else{
-            //     const token = await req.header('Authorization').replace('Bearer ', '');
-            //     const decodedToken = await firebase.admin.auth().verifyIdToken(token.toString());
-            //     const uid =  decodedToken.uid;
-            //     user = await firebase.admin.auth().getUser(uid);
-            // }
+            }else{
+                const token = await req.header('Authorization').replace('Bearer ', '');
+                const decodedToken = await firebase.admin.auth().verifyIdToken(token.toString());
+                const uid =  decodedToken.uid;
+                user = await firebase.admin.auth().getUser(uid);
+            }
         
 
         if (!user) {
